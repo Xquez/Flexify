3f4673871ae7ffdc20ff8cb823a7eec7da237f9d
Devnexys <bhaghyeshshah@gmail.com>
2025-10-06 20:34:08 +0530

    Intermediary commit

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,3 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+// export const firebaseConfig = {
+//   "projectId": "studio-5559843711-3d231",
+//   "appId": "1:686111410503:web:9ff384ca362924f07b6462",
+//   "apiKey": "AIzaSyAPKwTvUOdId8ZbZF3RmjyUGwrz0kpXbzw",
+//   "authDomain": "studio-5559843711-3d231.firebaseapp.com",
+//   "measurementId": "",
+//   "messagingSenderId": "686111410503"
+// };
+
 export const firebaseConfig = {
   "projectId": process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   "appId": process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
@@ -7,3 +20,7 @@ export const firebaseConfig = {
   "messagingSenderId": process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   "storageBucket": process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET
 };
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
